c916dd06444f3bac3374543b4f67778964e46d0d
Pratham Singh <ps3261712@gmail.com>
2026-02-26 17:33:39 +0530

    Time: 0 ms (100%), Space: 9 MB (80.8%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,32 +1,34 @@
 class Solution {
- public:
-  int myAtoi(string s) {
-    trim(s);
-    if (s.empty())
-      return 0;
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.length();
+        int sign = 1;
+        int result = 0;
 
-    const int sign = s[0] == '-' ? -1 : 1;
-    if (s[0] == '+' || s[0] == '-')
-      s = s.substr(1);
+        // skip whitespace
+        while(i < n && s[i] == ' '){
+            i++;
+        }
+        // check for sign
+        if(i < n && (s[i] == '+' || s[i] == '-')){
+            if(s[i] == '-')
+                sign = -1;
+            i++;
+        }
+        // read digits
+        while(i < n && isdigit(s[i])){
+            int digit = s[i] - '0';
 
-    long num = 0;
-
-    for (const char c : s) {
-      if (!isdigit(c))
-        break;
-      num = num * 10 + (c - '0');
-      if (sign * num < INT_MIN)
-        return INT_MIN;
-      if (sign * num > INT_MAX)
-        return INT_MAX;
+            if(result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > 7)){
+                if(sign == 1)
+                    return INT_MAX;
+                else
+                    return INT_MIN;
+            }
+            result = result * 10 + digit;
+            i++;
+        }
+        return sign * result;
     }
-
-    return sign * num;
-  }
-
- private:
-  void trim(string& s) {
-    s.erase(0, s.find_first_not_of(' '));
-    s.erase(s.find_last_not_of(' ') + 1);
-  }
 };
