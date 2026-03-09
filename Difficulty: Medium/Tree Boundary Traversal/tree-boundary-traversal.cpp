1f830f8bbb0164c24fff299a238b4919f3e4b0dc
Pratham Singh <ps3261712@gmail.com>
2026-03-09 12:52:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -0,0 +1,63 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+    bool isLeaf(Node* node) {
+        return !node->left && !node->right;
+    }
+
+    void addLeftBoundary(Node* root, vector<int> &res) {
+        Node* cur = root->left;
+        while (cur) {
+            if (!isLeaf(cur)) res.push_back(cur->data);
+            if (cur->left) cur = cur->left;
+            else cur = cur->right;
+        }
+    }
+
+    void addRightBoundary(Node* root, vector<int> &res) {
+        Node* cur = root->right;
+        vector<int> tmp;
+        while (cur) {
+            if (!isLeaf(cur)) tmp.push_back(cur->data);
+            if (cur->right) cur = cur->right;
+            else cur = cur->left;
+        }
+        for (int i = tmp.size()-1; i >= 0; --i) {
+            res.push_back(tmp[i]);
+        }
+    }
+
+    void addLeaves(Node* root, vector<int>& res) {
+        if (isLeaf(root)) {
+            res.push_back(root->data);
+            return;
+        }
+        if (root->left) addLeaves(root->left, res);
+        if (root->right) addLeaves(root->right, res);
+    }
+
+  public:
+    vector<int> boundaryTraversal(Node *root) {
+        vector<int> res;
+        if (!root) return res;
+        if (!isLeaf(root)) res.push_back(root->data);
+        addLeftBoundary(root, res);
+        addLeaves(root, res);
+        addRightBoundary(root, res);
+        return res;
+    }
+};
