0cbc98be83c56801c2aa3b6d169c41095c8f50e7
Pratham Singh <ps3261712@gmail.com>
2026-03-17 10:55:12 +0530

    Time: 7 ms (22.36%), Space: 12.3 MB (27.8%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,71 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+
+    Node* dfs(Node* node) {
+        if (!node) return NULL;
+
+        // If already cloned
+        if (mp.find(node) != mp.end()) {
+            return mp[node];
+        }
+
+        // Create clone
+        Node* clone = new Node(node->val);
+        mp[node] = clone;
+
+        // Clone neighbors
+        for (auto neighbor : node->neighbors) {
+            clone->neighbors.push_back(dfs(neighbor));
+        }
+
+        return clone;
+    }
+
+    Node* cloneGraph(Node* node) {
+        if (!node) return NULL;
+        return dfs(node);
+    }
+};
