17adbacdb7ea661c553ac5bf8c54600aeccf40f3
Pratham Singh <ps3261712@gmail.com>
2026-03-24 11:01:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -7,29 +7,31 @@ class Solution {
         adj[e[1]].push_back({e[0], e[2]}); // undirected
         }
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<int> dist(V);
-        for(int i = 0; i< V; i++)
-            dist[i] = 1e9;
-            
-            dist[src] = 0;
-            pq.push({0,src});
-            
-            while(!pq.empty()){
-                int dis = pq.top().first;
-                int node = pq.top().second;
-                pq.pop();
-                
-                for(auto it: adj[node]){
-                    int edgeWeight = it.second;
-                    int adjNode = it.first;
-                
-                    if(dis + edgeWeight < dist[adjNode]){
-                        dist[adjNode] = dis + edgeWeight;
-                        pq.push({dist[adjNode], adjNode});
-                    }
+        set<pair<int,int>> st;
+        vector<int> dist(V, 1e9);
+
+        st.insert({0, src});
+        dist[src] = 0;
+
+        while(!st.empty()) {
+            auto it = *(st.begin());
+            int dis = it.first;
+            int node = it.second;
+            st.erase(it);
+
+            for(auto it : adj[node]) {
+                int adjNode = it.first;
+                int edgW = it.second;
+
+                if(dis + edgW < dist[adjNode]) {
+                    if(dist[adjNode] != 1e9)
+                        st.erase({dist[adjNode], adjNode});
+
+                    dist[adjNode] = dis + edgW;
+                    st.insert({dist[adjNode], adjNode});
                 }
             }
+        }
         return dist;
     }
 };
