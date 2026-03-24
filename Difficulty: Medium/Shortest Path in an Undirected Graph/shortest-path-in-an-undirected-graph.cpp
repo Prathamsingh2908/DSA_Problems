549c2b247dee00f8a5af66cddf186b1ae75dd1d4
Pratham Singh <ps3261712@gmail.com>
2026-03-24 11:41:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Path in an Undirected Graph/shortest-path-in-an-undirected-graph.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+        vector<pair<int,int>> adj[n+1];
+        for(auto it : edges) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> dist(n+1, 1e9), parent(n+1);
+        for(int i = 1; i <= n; i++) parent[i] = i;
+        dist[1] = 0;
+        pq.push({0, 1});
+
+        while(!pq.empty()) {
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for(auto it : adj[node]) {
+                int adjNode = it.first;
+                int edW = it.second;
+                if(dis + edW < dist[adjNode]) {
+                    dist[adjNode] = dis + edW;
+                    pq.push({dist[adjNode], adjNode});
+                    parent[adjNode] = node;
+                }
+            }
+        }
+
+        if(dist[n] == 1e9) return {-1};
+        vector<int> path;
+        int node = n;
+        while(parent[node] != node) {
+            path.push_back(node);
+            node = parent[node];
+        }
+        path.push_back(1);
+        reverse(path.begin(), path.end());
+        
+        // insert distance at front
+        path.insert(path.begin(), dist[n]);
+        return path;
+    }
+};
